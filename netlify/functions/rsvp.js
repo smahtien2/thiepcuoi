18883e741c98d28e7eea72eb824ed72cc2ac527b
smahtien2 <manhtien301293@gmail.com>
2025-12-27 15:03:12 +0700

    Update rsvp.js

--- netlify/functions/rsvp.js
+++ netlify/functions/rsvp.js
@@ -1,45 +1,45 @@
-import { Client } from "pg";
-
 export const handler = async (event) => {
+
+  const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbwdgQmdBGOBvRerbP0hCyDOlJHxTDDMfn39oD9axngWJ8RZp86_BQEAC63bqJAXeBO4/exec";
+
+
+
   try {
-    if (event.httpMethod !== "POST") {
-      return { statusCode: 405, body: "Method Not Allowed" };
-    }
 
-    const { full_name, number_of_guests, attending, message } =
-      JSON.parse(event.body || "{}");
+    const data = JSON.parse(event.body);
 
-    const client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false }
+
+
+    // Gửi dữ liệu sang Google Sheet
+
+    await fetch(GOOGLE_SHEET_URL, {
+
+      method: "POST",
+
+      body: JSON.stringify(data)
+
     });
 
-    await client.connect();
 
-    await client.query(
-      `INSERT INTO rsvp_guests
-      (full_name, number_of_guests, attending, message)
-      VALUES ($1, $2, $3, $4)`,
-      [full_name, number_of_guests, attending, message]
-    );
-
-    await client.end();
 
     return {
+
       statusCode: 200,
-      body: JSON.stringify({ message: "🎉 Đã ghi nhận xác nhận của bạn!" })
+
+      body: JSON.stringify({ message: "Đã lưu vào Google Sheet!" })
+
     };
 
   } catch (err) {
-    console.error("RSVP ERROR:", err.message);
 
     return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "❌ Lỗi server",
-        error: err.message
-      })
-    };
-  }
-};
 
+      statusCode: 500,
+
+      body: JSON.stringify({ error: err.message })
+
+    };
+
+  }
+
+};
